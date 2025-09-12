c7d60416b84b9c669af558d1e4a4a994e9693364
kisara579 <kisarabeddawala@gmail.com>
2025-09-12 07:46:42 +0530

    complete lab 1

--- src/LW_01/Q5.java
+++ src/LW_01/Q5.java
@@ -1,7 +1,26 @@
 package LW_01;
 
+import java.util.Scanner;
+
 public class Q5 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter inner radius ? ");
+        int ri = input.nextInt();
+
+        System.out.println("Enter outer radius ? ");
+        int ro = input.nextInt();
+
+        Circle innerCircle = new Circle(ri);
+        Circle outerCircle = new Circle(ro);
+
+        double innerCircleArea = innerCircle.computeArea();
+        double outerCircleArea = outerCircle.computeArea();
+
+        double shadedArea = outerCircleArea - innerCircleArea;
+
+        System.out.println("Shaded Area "+shadedArea);
 
     }
 }
