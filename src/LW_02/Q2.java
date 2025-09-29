4784b7d75c08f19fe20c771f3180d995bef9d303
kisara579 <kisarabeddawala@gmail.com>
2025-09-29 15:45:08 +0530

    change question 2 answer

--- src/LW_02/Q2.java
+++ src/LW_02/Q2.java
@@ -5,23 +5,40 @@ import java.util.Scanner;
 public class Q2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int number = 0;
+        int number;
 
-        while(true){
-            int count = 0;
 
+        do {
             System.out.println("Enter the Number : ");
-            number =  input.nextInt();
+            number = input.nextInt();
 
-            if(number<0){
+
+            if (number < 0) {
                 break;
             }
+            int digit = countDigits(number);
+            System.out.println("Number has " + digit + " digits.");
+        } while (true);
 
-            while(number!=0) {
-                number = number / 10;
-                count++;
-            }
-            System.out.println("Number has "+count+" digits.");
+    }
+
+    public static int countDigits(int number) {
+        int count = 0;
+
+        if (number == 0) {
+            count = 1;
+        }
+
+        while (number != 0) {
+            number = number / 10;
+            count++;
+        }
+        return count;
         }
     }
-}
+
+
+
+
+
+
